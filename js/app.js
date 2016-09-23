d54c6f5b67e1b732f97e722a4052ff28a8d8edeb
anniekbh <anniekbh@gmail.com>
2016-09-23 09:23:23 -0500

    modificando archivos

--- js/app.js
+++ js/app.js
@@ -5,22 +5,28 @@ window.addEventListener("load", function() {
 	var crearMensaje = document.getElementById("mensaje");
 	
 	lista.addEventListener("click", function() {
-		lista.style.display = "none";
-		listaNueva.style.display = "block";
-	});
-/*
-	boton.addEventListener("click", function() {
-		boton.addEventListener("click", function(e) {
+			lista.style.display = "none";
+			listaNueva.style.display = "block";
+		});
+	boton.addEventListener("click", function(e) {
 		e.preventDefault();
 		mostrarMensaje(crearMensaje.value);
 		crearMensaje.value = "";
-		boton.disabled = true; 
+		boton.disabled = true;
+		listaNueva.style.display = "none";
 	});
-		function mostrarMensaje(crearMensaje){
+
+	function mostrarMensaje(crearMensaje){
+		
 		var newMensaje = document.createElement("div");
-	    var mostrarMensaje = document.createTextNode(crearMensaje);
+        var mostrarMensaje = document.createTextNode(crearMensaje);
 	    newMensaje.appendChild(mostrarMensaje);
 	    var placeText = document.getElementById("salida");
 	    placeText.insertBefore(newMensaje, placeText.childNodes[0]);
-	}*/
+
+	    var agregar = document.createElement("div");
+        agregar.classList.add("listaMas");
+        agregar.innerHTML= "Añadir una lista...";
+        newMensaje.insertBefore(arroba, newMensaje.childNodes[1]);
+	};
 });
